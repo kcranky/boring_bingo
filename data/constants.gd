c43fb1092101e5b8def25d5bf0aaec164d822bf3
Keegan Crankshaw <kcranky@gmail.com>
2025-10-05 11:49:37 +0100

    Initial commit with skeleton.

---
+++ data/constants.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# See the following image for an example
+
+# Define the size of the bingo sheets
+const grids_per_ticket : int = 6
+const rows_per_grid : int = 3
+const cols_per_grid : int = 9
---
+++ scripts/generate_ticket.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+func generate_ticket(rng_seed : int):
+    for grid in CONSTANTS.grids_per_ticket:
+        for row in CONSTANTS.rows_per_ticket:
+            pass
